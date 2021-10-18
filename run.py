fcf06050866ab33e33bff3592108147adb439ba0
NgoniDukeMoyo <dukemoyo7@gmail.com>
2021-10-18 07:22:53 +0000

    set-up heroku and wire-up heroku in gitpod

--- run.py
+++ run.py
@@ -7,12 +7,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", page_title="Home")
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", page_title="About")
 
 
 if __name__ == "__main__":
