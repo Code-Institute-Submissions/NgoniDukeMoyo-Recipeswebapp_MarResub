dffb44e2c36e3ef49bf7e2c77e863eb63824225b
NgoniDukeMoyo <dukemoyo7@gmail.com>
2021-10-22 13:08:02 +0000

    set-up to do status

--- todoapp/main/routes.py
+++ todoapp/main/routes.py
@@ -1,7 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request
+
+from todoapp.extentions import mango
 
 main = Blueprint('__name__')
 
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/add_todo', methods=['POST'])
+def add_todo():
+    todo_collection = mango.db.todos
+    todo_item = request.form.get('get-todo')
+    todo_collection.insert_one({'text': todo_item, 'complete': False})
+    return redirect(url_for('main.index'))
