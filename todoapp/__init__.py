713cd2e0c0b1c514ed0c4832fad3fa48ef7fb413
NgoniDukeMoyo <dukemoyo7@gmail.com>
2021-10-22 12:20:05 +0000

    connect my to dos database

--- todoapp/__init__.py
+++ todoapp/__init__.py
@@ -5,6 +5,8 @@ from .extentions import mango
 def create_app():
     app = flask(__name__)
 
+    app.config['MANGO_URI'] = 'mongodb+srv://Duke:Kingdong20@recipewebappcluster.crmag.mongodb.net/mydb?retryWrites=true&w=majority'
+
     mango.init_app(app)
 
     return app
