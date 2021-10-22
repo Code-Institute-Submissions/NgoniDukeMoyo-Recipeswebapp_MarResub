60f0aa2d4db8be925b8a3e2a537c710328aa2e30
NgoniDukeMoyo <dukemoyo7@gmail.com>
2021-10-22 13:27:51 +0000

    to dos as links set-up

--- todoapp/main/routes.py
+++ todoapp/main/routes.py
@@ -6,11 +6,17 @@ main = Blueprint('__name__')
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    todo_collection = mango.db.todosapp
+    todosapp = todo_collection.find()
+    return render_template('index.html', todo=todos)
 
 @main.route('/add_todo', methods=['POST'])
 def add_todo():
-    todo_collection = mango.db.todos
+    todo_collection = mango.db.todosapp
     todo_item = request.form.get('get-todo')
     todo_collection.insert_one({'text': todo_item, 'complete': False})
+    return redirect(url_for('main.index'))
+
+@main.route('/complete_todo/<oid>')
+def complete_todo(oid):
     return redirect(url_for('main.index'))
