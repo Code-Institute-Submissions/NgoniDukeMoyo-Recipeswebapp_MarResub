e1558c25f3bf112c176ffe3d839eb365869f5659
NgoniDukeMoyo <dukemoyo7@gmail.com>
2021-10-23 23:41:17 +0000

    fire upp to the data base and test adding todos from env

--- templates/todoapp/extentions.py
+++ templates/todoapp/extentions.py
@@ -1,3 +1,18 @@
-from flask_pymango import flask_PyMango 
+from flask_pymango import blueprint, render_template, redirect, url_for, request
 
-mango = PyMango() 
+from todosapp.extentions import mango
+
+main = Blueprint('main', __name__)
+
+
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+@main.route('/add_todo', methods=['POST'])
+def add_todo():
+    todos_collection = mango.db.todosapp
+    todo_item = request.form.get('add-todo')
+    todos_collection.insert_one({'text': todo_item, 'complete' : False})
+    return redirect(url_for('main.index'))
--- templates/todoapp/mango.py
+++ templates/todoapp/mango.py
@@ -5,8 +5,8 @@ if os.path.exists("env.py"):
 
 
 MONGO_URI = os.environ.get("MONGO_URI")
-DATABASE = "myRecipesWebAppSyntax"
-COLLECTION = "FoodRecipesSyntax"
+DATABASE = "mydb"
+COLLECTION = "todosapp"
 
 
 def mongo_connect(url):
