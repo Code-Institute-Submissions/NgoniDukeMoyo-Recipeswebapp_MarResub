819e2df6b274f3b046dc1eb528c72486c8bfb258
NgoniDukeMoyo <dukemoyo7@gmail.com>
2022-03-13 23:00:51 +0000

    final files check before push

--- main/app.py
+++ main/app.py
@@ -4,21 +4,24 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-#app.config['MONGO_URI'] = 'mongodb+srv://Duke:Kingdong20@cluster0.jxvdd.mongodb.net/mydb?retryWrites=true&w=majority'
+
 mongo = PyMongo(app)
 
 todos = mongo.db.todos
+
 
 @app.route('/')
 def index():
     saved_todos = todos.find()
     return render_template('index.html', todos=saved_todos)
 
+
 @app.route('/add', methods=['POST'])
 def add_todo():
     new_todo = request.form.get('new-todo')
     todos.insert_one({'text' : new_todo, 'complete' : False})
     return redirect(url_for('index'))
+
 
 @app.route('/complete/<oid>')
 def complete(oid):
@@ -27,10 +30,12 @@ def complete(oid):
     todos.save(todo_item)
     return redirect(url_for('index'))
 
+
 @app.route('/delete_completed')
 def delete_completed():
     todos.delete_many({'complete' : True})
     return redirect(url_for('index'))
+
 
 @app.route('/delete_all')
 def delete_all():
