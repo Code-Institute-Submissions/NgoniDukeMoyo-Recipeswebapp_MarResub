b24de87a254be574d1945b0e68c811ece726e84b
NgoniDukeMoyo <dukemoyo7@gmail.com>
2022-03-13 21:54:34 +0000

    add docstrings to run.py and env using docly

--- run.py
+++ run.py
@@ -6,8 +6,7 @@ if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
-#app.secret_key = os.environ.get("SECRET_KEY")
-#app.config['MONGO_URI'] = 'mongodb+srv://Duke:Kingdong20@recipewebappcluster.crmag.mongodb.net/mydb?retryWrites=true&w=majority'
+
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 mongo = PyMongo(app)
@@ -19,12 +18,14 @@ def index():
     saved_todos = todos.find()
     return render_template('index.html', todos=saved_todos)
 
+"""Add todo items to database """
 @app.route('/add', methods=['POST'])
 def add_todo():
     new_todo = request.form.get('new-todo')
     todos.insert_one({'text' : new_todo, 'complete' : False})
     return redirect(url_for('index'))
 
+"""scratch out link of completed item and redirect back to index"""
 @app.route('/complete/<oid>')
 def complete(oid):
     todo_item = todos.find_one({'_id': ObjectId(oid)})
@@ -36,11 +37,13 @@ def complete(oid):
     print(todo_item)
     return redirect(url_for('index'))
 
+"""Removes completed to dos"""
 @app.route('/delete_completed')
 def delete_completed():
     todos.delete_many({'complete' : True})
     return redirect(url_for('index'))
 
+"""Removes all to dos"""
 @app.route('/delete_all')
 def delete_all():
     todos.delete_many({})
@@ -51,4 +54,4 @@ if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
-        debug=True)
+        debug=False)
